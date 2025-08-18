ed160cd60586e3a5e12289a375feee6c38a35e50
Dan Drennan <danjdrennan@gmail.com>
2025-08-18 13:52:47 -0500

    model: reparameterize `update_train_data` to match other interfaces #1
    Originally wrote `CovariateTransportMap.update_train_data` to accept
    args
    - `new_x: jax.Array(num_samples, x_dim)`
    - `new_y: jax.Array(num_locs, num_samples)`
    
    That is basically a transpose of every other interface we provide. This
    commit transposes that so that the interface uses `new_x:
    jax.Array(num_samples, num_locs)`, which more closely matches all of the
    other interface builders.
    
    An example use case is
    
    ```python
    rep_num = 0 # the samples have shape (num_reps, num_samples, num_locs)
    model.update_train_data(ds.covariates, ds.samples[rep_num])
    ```
    
    The example above matches with the `getting-started.ipynb` notebook
    docs. But be aware of the shapes of your data because we aren't
    validating that internally yet.

--- src/batram_cov/model.py
+++ src/batram_cov/model.py
@@ -142,19 +142,19 @@ class CovariateTransportMap:
 
         Args:
         new_x: an x value with shape (num_samples, x_dim)
-        new_samples: fields with shape (num_locs, num_samples)
+        new_samples: fields with shape (num_samples, num_locs)
 
         Returns:
         None
         """
         graph, state = nnx.split(self.model)
-        _response, conditioning_set, covariates, _, nn_idx, _ = state["data"].values()
+        _response, condsets, covariates, _, nn_idx, _ = state["data"].values()
 
         new_x = np.broadcast_to(new_x, (covariates.value.shape[0], *new_x.shape))
-        new_condsets = new_samples[nn_idx.value].transpose(0, -1, -2)
+        new_condsets = new_samples[:, nn_idx.value].transpose(1, 0, -1)
         x = jnp.concat([covariates.value, new_x], axis=-2)
-        y = jnp.concat([_response.value, new_samples], axis=-1)
-        condsets = jnp.concat([conditioning_set.value, new_condsets], axis=-2)
+        y = jnp.concat([_response.value, new_samples.T], axis=-1)
+        condsets = jnp.concat([condsets.value, new_condsets], axis=-2)
 
         state["data"]["_response"].value = y
         state["data"]["covariates"].value = x
