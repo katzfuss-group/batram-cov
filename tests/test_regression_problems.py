a26570239aa87bdd45731cf232fbebfc94e08f24
Dan Drennan <danjdrennan@gmail.com>
2026-04-12 20:48:35 -0500

    fix(tests): Builds valid(-ish) data fixtures
    Problem: The original tests diverged from the implementation and failed.
    The test and underlying object provide no hints of how to rectify this.
    
    Solution: Modifies the original fixture to mirror the constructions used
    in application (making it a regression test for future changes). Also
    adds a new fixture that represents an alternative construction that
    would be valid.
    
    The two fixtures are related as follows: The `one_point` construction
    mirrors what gets built in the application code one module at a time.
    After a list of problems are constructed, they are merged using
    `flax.nnx.merge(modules_list)` to build the problems used in practice.
    
    This is a short fix that makes more clear how to construct a valid
    state. More attention should be given to this interface, namely to
    guarantee that invalid states do not initialize.
    
    TODO: Add docs to module and validate data during initialization. Stack
    traces in the materialized objects are unhelpful for understanding why
    the previous tests failed. It would be daunting as a user to work with
    or modify this code in any way. Docs and error handling/automatic
    shaping would alleviate a lot of this difficulty.

--- tests/test_regression_problems.py
+++ tests/test_regression_problems.py
@@ -1,3 +1,5 @@
+from collections.abc import Callable
+
 import jax
 import jax.numpy as jnp
 import pytest
@@ -6,20 +8,58 @@ import batram_cov.regression_problem as rp
 
 
 @pytest.fixture
-def data() -> rp.TMDataModule:
+def one_point() -> rp.TMDataModule:
+    """Used in `test_one_point`."""
+
     key = jax.random.key(0)
-    position = jnp.array(100, dtype=int)
-    response = jax.random.normal(key, shape=(100,))
-    conditioning_set = jax.random.normal(key, shape=(100, 30))
-    covariates = jax.random.normal(key, shape=(100,))
-    dist_nn = 1 / (jnp.arange(100) + 1)
-    nn_idx = jnp.arange(100, dtype=int)
+    position = jnp.array(0, dtype=int)
+    response = jax.random.normal(key, shape=(1,))
+    conditioning_set = jax.random.normal(key, shape=(1, 30))
+    covariates = jax.random.normal(key, shape=(1, 1))
+    dist_nn = 1 / (position + 1)
+    nn_idx = jnp.zeros((1, 30), dtype=int).at[:, 1:].set(-1)
     data = rp.TMDataModule(
         position, response, conditioning_set, covariates, dist_nn, nn_idx
     )
-
     return data
 
 
-def test_data(data):
-    assert data.response([0]).shape == (), data.response([0]).shape
+@pytest.fixture
+def n_points() -> rp.TMDataModule:
+    """Used in `test_n_points`."""
+
+    key = jax.random.key(0)
+    position = jnp.arange(100, dtype=int).reshape(-1, 1)
+    response = jax.random.normal(key, shape=(100, 5))
+    conditioning_set = jax.random.normal(key, shape=(100, 30))
+    covariates = jax.random.normal(key, shape=(5, 1))
+    dist_nn = 1 / (position + 1)
+    nn_idx = jnp.zeros((100, 30), dtype=int).at[:, 1:].set(-1)
+    data = rp.TMDataModule(
+        position, response, conditioning_set, covariates, dist_nn, nn_idx
+    )
+    return data
+
+
+def test_one_point(one_point):
+    def _check_shape(
+        field: Callable, mb_idx: int, expect: tuple[int | None, ...]
+    ) -> None:
+        assert field(mb_idx).shape == expect, field(mb_idx).shape
+
+    _check_shape(one_point.response, 0, ())
+    _check_shape(one_point.input_f, 0, (31,))
+    _check_shape(one_point.input_g, 0, (1,))
+
+
+def test_n_points(n_points):
+    # NOTE: mb_idx must be provided as an array type based on current jax
+    # version (0.5.0) for lax interfaces.
+    def _check_shape(
+        field: Callable, mb_idx: jax.Array, expect: tuple[int | None, ...]
+    ) -> None:
+        assert field(mb_idx).shape == expect, field(mb_idx).shape
+
+    _check_shape(n_points.response, jnp.array([0]), (1, 5))
+    _check_shape(n_points.input_f, jnp.array([0]), (1, 31))
+    _check_shape(n_points.input_g, jnp.array([0]), (1, 1))
